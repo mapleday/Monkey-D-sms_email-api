1d98a997c91c412734d85fa948586b0f11faad67
chenshouqin <shouqinchen@sohu-inc.com>
2016-01-27 14:34:27 +0800

    添加超时提醒特殊规则

--- src/main/java/com/sohu/sms_email/service/service/impl/EmailServiceImpl.java
+++ src/main/java/com/sohu/sms_email/service/service/impl/EmailServiceImpl.java
@@ -4,24 +4,18 @@ import com.sohu.sms_email.service.EmailService;
 import com.sohu.snscommon.utils.EmailUtil;
 import org.springframework.stereotype.Component;
 
-import javax.mail.MessagingException;
-
 /**
  * Created by Gary on 2015/10/21.
  */
 @Component("emailService")
 public class EmailServiceImpl implements EmailService {
 
-    public void sendSimpleEmail(String subject, String text, String[] to) {
+    public void sendSimpleEmail(String subject, String text, String[] to) throws Exception {
         EmailUtil.sendSimpleEmail(subject, text, to);
     }
 
     @Override
-    public void sendHtmlEmail(String subject, String text, String[] to) {
-        try {
+    public void sendHtmlEmail(String subject, String text, String[] to) throws Exception {
             EmailUtil.sendHtmlEmail(subject, text, to);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
     }
 }
