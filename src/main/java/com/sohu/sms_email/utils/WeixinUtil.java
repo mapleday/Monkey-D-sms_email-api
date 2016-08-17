93df1b2269e5a710f177108de1a15cbdd161edc7
jinyingshi <jinyingshi@sohu-inc.com>
2016-08-17 16:20:48 +0800

    短信报警切换到微信

--- src/main/java/com/sohu/sms_email/utils/WeixinUtil.java
+++ src/main/java/com/sohu/sms_email/utils/WeixinUtil.java
@@ -1,5 +1,6 @@
 package com.sohu.sms_email.utils;
 
+import com.alibaba.druid.util.StringUtils;
 import com.sohu.snscommon.utils.LOGGER;
 import com.sohu.snscommon.utils.constant.ModuleEnum;
 import com.sohu.snscommon.utils.http.HttpClientUtil;
@@ -12,33 +13,71 @@ import java.util.Map;
  * 发送微信
  */
 public class WeixinUtil {
+    /**
+     * 微信地址
+     */
     private static final String weixinUrl = "http://mtpc.sohu.com/wmsnotify/wmsnotify4sohu.php";
+    /**
+     * httpclient
+     */
     private static HttpClientUtil httpClientUtil = HttpClientUtil.create("weixinUtil", "sendMessage", 3000);
 
-    public static boolean sendMessage(String mobile_list, String message_post) {
+
+    /**
+     * 发送微信
+     *
+     * @param mobileList  手机号列表 逗号分割
+     * @param messagePost 发送内容
+     * @return ture 发送成功
+     */
+    public static boolean sendMessage(String mobileList, String messagePost) {
+        if (StringUtils.isEmpty(mobileList)) {
+            return false;
+        }
+        String[] mobileArr = mobileList.split(",");
+        for (String mobile : mobileArr) {
+            for (int i = 0; i < 3; i++) {
+                boolean success = send(mobile, messagePost);
+                if (success) {
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     * 发送微信
+     *
+     * @param mobile  一个手机号
+     * @param content 发送内容
+     * @return ture 发送成功
+     */
+    private static boolean send(String mobile, String content) {
         try {
             Map<String, String> params = new HashMap();
-            params.put("mobile_list", mobile_list);
-            params.put("message_post", message_post);
+            params.put("mobile_list", mobile);
+            params.put("message_post", content);
             String result = httpClientUtil.getByUtf(weixinUrl, params);
             System.out.println(result);
             if (result == null) {
-                LOGGER.errorLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", message_post, message_post, new Exception("send weixin null"));
+                LOGGER.errorLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", mobile, content, new Exception("send weixin null"));
                 return false;
             } else if (!result.contains("发送成功")) {
-                LOGGER.errorLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", message_post, message_post, new Exception("send weixin null"));
+                LOGGER.errorLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", mobile, content, new Exception("send weixin null"));
                 return false;
             }
-            LOGGER.buziLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", mobile_list + "#" + message_post, result);
+            LOGGER.buziLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", mobile + "#" + content, result);
         } catch (Exception e) {
-            LOGGER.errorLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", message_post, message_post, e);
+            LOGGER.errorLog(ModuleEnum.SMS_EMAIL_SERVICE, "WeixinUtil.sendMessage", mobile, content, e);
             return false;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        boolean test = WeixinUtil.sendMessage("18910556026", "test");
+        boolean test = WeixinUtil.sendMessage("18511871276,18910556026", "test2");
         System.out.println(test);
     }
 }
